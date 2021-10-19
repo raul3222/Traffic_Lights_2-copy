508a23ca50f55cecea0865c430724f8a829dbb02
R.Shafigin <sh_raul08@mail.ru>
2021-10-19 21:11:38 +0200

    Initial Commit

---
+++ Traffic_Lights_2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Traffic_Lights_2
+//
+//  Created by Raul Shafigin on 19.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
