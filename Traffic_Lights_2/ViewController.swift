0414a522c2ba92e8fc1d33230cb6c59d11da7174
R.Shafigin <sh_raul08@mail.ru>
2021-10-19 22:44:12 +0200

    last worked

--- Traffic_Lights_2/ViewController.swift
+++ Traffic_Lights_2/ViewController.swift
@@ -9,11 +9,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redLightView: UIView!
+    @IBOutlet var yellowLightView: UIView!
+    @IBOutlet var greenLightView: UIView!
+    @IBOutlet var buttonStart: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        buttonStart.layer.cornerRadius = 10
+       
+    }
+    
+    override func viewDidAppear(_ animated: Bool)  {
+        redLightView.layer.cornerRadius = redLightView.frame.width / 2
+        yellowLightView.layer.cornerRadius = yellowLightView.frame.height / 2
+        greenLightView.layer.cornerRadius = greenLightView.frame.width / 2
+        
+        buttonStart.setTitle("\(redLightView.frame.size.width)", for: .normal)
     }
 
-
+    @IBAction func buttonStartPressed() {
+        buttonStart.setTitle("NEXT", for: .normal)
+        
+        if redLightView.alpha < 1 && yellowLightView.alpha != 1 {
+            redLightView.alpha = 1
+            greenLightView.alpha = 0.3
+        } else if yellowLightView.alpha < 1 && greenLightView.alpha != 1 {
+            yellowLightView.alpha = 1
+            redLightView.alpha = 0.3
+        } else {
+            greenLightView.alpha = 1
+            yellowLightView.alpha = 0.3
+        }
+    }
+    
 }
+
+
+
 
