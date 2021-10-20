3016502ce4b92107e59e7d16e3b7dcb3cc09ddd5
R.Shafigin <sh_raul08@mail.ru>
2021-10-20 18:05:20 +0200

    final

--- Traffic_Lights_2/ViewController.swift
+++ Traffic_Lights_2/ViewController.swift
@@ -13,36 +13,37 @@ class ViewController: UIViewController {
     @IBOutlet var yellowLightView: UIView!
     @IBOutlet var greenLightView: UIView!
     @IBOutlet var buttonStart: UIButton!
-    
+    var currentColor = ""
     override func viewDidAppear(_ animated: Bool)  {
         redLightView.layer.cornerRadius = redLightView.frame.width / 2
         yellowLightView.layer.cornerRadius = yellowLightView.frame.width / 2
         greenLightView.layer.cornerRadius = greenLightView.frame.width / 2
-        
-       
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         buttonStart.layer.cornerRadius = 10
-       
+        currentColor = "red"
     }
     
    
 
     @IBAction func buttonStartPressed() {
         buttonStart.setTitle("NEXT", for: .normal)
-        
-        if redLightView.alpha < 1 && yellowLightView.alpha != 1 {
+        switch currentColor {
+        case "red":
             redLightView.alpha = 1
             greenLightView.alpha = 0.3
-        } else if yellowLightView.alpha < 1 && greenLightView.alpha != 1 {
+            currentColor = "yellow"
+        case "yellow":
             yellowLightView.alpha = 1
             redLightView.alpha = 0.3
-        } else {
+            currentColor = "green"
+        case "green":
             greenLightView.alpha = 1
             yellowLightView.alpha = 0.3
+            currentColor = "red"
+        default: break
         }
     }
     
